2b7c6c7d3bbe8ea2ac82084e49e565333b3da613
Abdullateef Dauda <51302756+abdul-o@users.noreply.github.com>
2022-06-23 18:53:43 +0100

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let Name = "Abdullateef Dauda"
+let hight = 1.7
+let country = "Nigeria"
+
+console.log( "I am" + " " + Name)
+console.log( "My hight is" + " " + hight)
+console.log( "My country is" + " " + country)
+
+
